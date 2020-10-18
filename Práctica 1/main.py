b6e4e68727bdce8642eb06fa7ae9892be6f7808e
Claudia <50269025+clau-hb9@users.noreply.github.com>
2020-10-18 12:47:31 +0200

    Adaline

--- Práctica 1/main.py
+++ Práctica 1/main.py
@@ -1,49 +1,53 @@
-""" Imports """
 from adaline import Adaline
 import csv
 import os
 import numpy as np
-import matplotlib.pyplot as plt
-
-""" Paths data """
-current_folder = os.path.dirname(os.path.abspath(__file__))
-files_initial_path = 'DatosProcesados/'
-files_output_path = 'Salidas/'
-file_extension = '.txt'
 
 
+""" Variables globales """
+directorio_actual = os.path.dirname(os.path.abspath(__file__))
+carpeta_datosProcesados = 'DatosProcesados/'
+carpeta_salidasProducidas = 'salidasProducidas/'
+extension = '.txt'
 
-""" File to vector: no headers values to float """
-def leer(directorio, fichero, tipo):
+
+
+""" Función para leer un fichero csv y devolvelo en un array """
+def leer(carpeta_datosProcesados, fichero):
     # Abrimos el fichero
-    with open(os.path.join(current_folder, directorio + fichero + tipo + file_extension), mode='r') as file_data:
+    with open(os.path.join(directorio_actual, carpeta_datosProcesados + fichero + extension), mode='r') as fichero_csv:
         # Lo leemos
-        reader = csv.reader(file_data, delimiter=',')
-        #reader = np.array(reader)
-        # Return vector of rows where each row is vector of float values
-        # Nos saltamos las categorias
-        #datos = []
+        csv_reader = csv.reader(fichero_csv, delimiter=',')
         # Quitamos las cabeceras
-        next(reader)
-        return [[float(value) for value in row] for row in reader]
-        """for i, fila in enumerate(reader):
-            for j, valor in enumerate (fila):
-                datos[i,j] = float(valor)
-        return datos"""
+        next(csv_reader)
+        return [[float(value) for value in row] for row in csv_reader]
+       
+
+
+""" Test different learning rates to compare different models, sort them by Test MAE and find best of them"""
+def test_learning_rates(nombre_problema, datos_entrenamiento, datos_validacion, razones_aprendizaje):
+    #models = []
+    # Para cada razon de entrenamiento realizamos un modelo
+    for razon_aprendizaje in razones_aprendizaje:
+        # Mostramos por consola que razon de entrenamiento estamos utilizando
+        print("Probando una razón de entrenamiento = " + str(razon_aprendizaje))
+        #print("  - Starts:",datetime.now().strftime("%H:%M:%S"))
+
+        # Train model (change graph=True and/or debug=True if needed)
+        adaline = Adaline(id=nombre_problema, razon_aprendizaje=razon_aprendizaje, datos_entrenamiento=datos_entrenamiento, datos_validacion=datos_validacion, graph=False, debug=False).entrenamiento()
+        print(adaline.pesos)
 
 """ Main """
 def main():
     #problem = 'DatosEntrada'
-    # Trainind and validation data
-    datos_entrenamiento = leer(files_initial_path, 'DatosEntrada', '_entrenamiento')
-    #datos_validacion = leer(files_initial_path, 'DatosEntrada', '_validacion')
+    # Leemos los ficheros de entrenamiento y de validacion que hemos creado
+    datos_entrenamiento = leer(carpeta_datosProcesados, 'DatosEntrada_entrenamientoPrueba')
+    datos_validacion = leer(carpeta_datosProcesados, 'DatosEntrada_validacionPrueba')
 
-    print(datos_entrenamiento)
-    # Learning rates to test
-    #learning_rates = [0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001, 0.000005, 0.000001]
+    # Lista con las distintas razones de aprendizaje
+    razones_aprendizaje = [0.1]
 
-    # Whole process start
-    #test_learning_rates(problem, training, validation, learning_rates)
+    test_learning_rates('DatosEntrada', datos_entrenamiento, datos_validacion, razones_aprendizaje)
 
 
 if __name__ == '__main__':
