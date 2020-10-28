5e6ca2db9c2e4e0fecbf1bb43bd0653ce96c5407
Claudia <50269025+clau-hb9@users.noreply.github.com>
2020-10-28 10:21:04 +0100

    Update ScriptR.r

--- Práctica 1/Perceptrón multicapa/DatosEntrada - Script/ScriptR.r
+++ Práctica 1/Perceptrón multicapa/DatosEntrada - Script/ScriptR.r
@@ -6,18 +6,29 @@ MSE <- function(pred,obs) {sum((pred-obs)^2)/length(obs)}
 MAE <- function(pred,obs) {sum(abs(pred-obs)/length(obs))}
 
 
+
 #CARGA DE DATOS
 # se supone que los ficheros tienen encabezados
- trainSet <- read.csv("DatosEntrada_entrenamiento.txt",dec=".",sep=",",header = T)
- validSet <- read.csv( "DatosEntrada_validacion.txt",dec=".",sep=",",header = T)
- testSet  <- read.csv("DatosEntrada_test.txt",dec=".",sep=",",header = T)
+trainSet <- read.csv("DatosEntrada_entrenamiento.txt",dec=".",sep=",",header = T)
+validSet <- read.csv( "DatosEntrada_validacion.txt",dec=".",sep=",",header = T)
+testSet  <- read.csv("DatosEntrada_test.txt",dec=".",sep=",",header = T)
+
+#trainSet <- read.table("trainParab.dat")
+#validSet <- read.table( "testParab.dat")
+#testSet <- read.table( "testParab.dat")
+
+
+salida <- ncol (trainSet)   #num de la columna de salida
+
+
+
+
+
 #SELECCION DE LOS PARAMETROS
-
-topologia        <-  c(8, 15, 10) #PARAMETRO DEL TIPO c(A,B,C,...,X) A SIENDO LAS NEURONAS EN LA CAPA OCULTA 1, B LA CAPA 2 ...
-razonAprendizaje <- 0.8 #NUMERO REAL ENTRE 0 y 1
-ciclosMaximos    <- 2500 #NUMERO ENTERO MAYOR QUE 0
-
-
+topologia        <-  c(10, 30) #PARAMETRO DEL TIPO c(A,B,C,...,X) A SIENDO LAS NEURONAS EN LA CAPA OCULTA 1, B LA CAPA 2 ...
+razonAprendizaje <- 0.1 #NUMERO REAL ENTRE 0 y 1
+ciclosMaximos    <- 2000 #NUMERO ENTERO MAYOR QUE 0
+seed            <- 9
 
 #EJECUCION DEL APRENDIZAJE Y GENERACION DEL MODELO
 
@@ -30,16 +41,14 @@ model <- mlp(x= trainSet[,-salida],
              maxit=ciclosMaximos,
              learnFuncParams=c(razonAprendizaje),
              shufflePatterns = F
-             )
+)
 
 #GRAFICO DE LA EVOLUCION DEL ERROR
-
 plotIterativeError(model)
 
 # DATAFRAME CON LOS ERRORES POR CICLo: de entrenamiento y de validacion
 iterativeErrors <- data.frame(MSETrain= (model$IterativeFitError/ nrow(trainSet)),
                               MSEValid= (model$IterativeTestError/nrow(validSet)))
-
 
 ######################################################################
 #SE OBTIENE EL NuMERO DE CICLOS DONDE EL ERROR DE VALIDACION ES MINIMO
@@ -62,8 +71,7 @@ model <- mlp(x= trainSet[,-salida],
 plotIterativeError(model)
 
 iterativeErrors1 <- data.frame(MSETrain= (model$IterativeFitError/ nrow(trainSet)),
-                              MSEValid= (model$IterativeTestError/nrow(validSet)))
-
+                               MSEValid= (model$IterativeTestError/nrow(validSet)))
 
 #CALCULO DE PREDICCIONES
 prediccionesTrain <- predict(model,trainSet[,-salida])
@@ -78,18 +86,6 @@ errors <- c(TrainMSE= MSE(pred= prediccionesTrain,obs= trainSet[,salida]),
             TestMSE=  MSE(pred= prediccionesTest ,obs=  testSet[,salida]),
             TestMAE= MAE(pred= prediccionesTest, obs=  testSet[,salida]))
 errors
-
-
-
-
- #trainSet <- read.table("trainParab.dat")
- #validSet <- read.table( "testParab.dat")
- #testSet <- read.table( "testParab.dat")
-
-
-salida <- ncol (trainSet)   #num de la columna de salida
-
-
 
 
 
